d9d0353715a1e4cca241b55ef31efab718522ebb
Adrian-007391 <101372036+Adrian-007391@users.noreply.github.com>
2022-06-28 11:53:49 -0500

    agregue los minibuttons en music

--- src/Pages/Code/Code.tsx
+++ src/Pages/Code/Code.tsx
@@ -11,9 +11,9 @@ function Code(){
 no eh vuelto atras. Eh utilizado lenguajes como python, javascript, typescript, c++, C#, R y librerias
 como React. Y me apasiona mucho el tema de las comatizaciones y el diseño web. Por eso, muchos de mis proyectos se enfocan en estas dos cosas'/>
                  <div className='Code__div--minibuttoncontainers'>
-                       <Minibutton texto = "Mi editor de  markdown" link ="https://adrian-007391.github.io/md_editor_react/" icono ={<AiFillFileMarkdown className='Code__icon'/>}/>
+                       <Minibutton texto = "Mi editor de  markdown" link ="https://adrian-007391.github.io/md_editor_react/" icono ={<AiFillFileMarkdown className='icon'/>}/>
 
-                       <Minibutton texto = "Mi Calculadora con python" icono = {<BsFillCalculatorFill className='Code__icon'/>} link= "https://github.com/Adrian-007391/Aplicacion_calculadora_python"/>
+                       <Minibutton texto = "Mi Calculadora con python" icono = {<BsFillCalculatorFill className='icon'/>} link= "https://github.com/Adrian-007391/Aplicacion_calculadora_python"/>
                  </div>
            </div>
       );
--- src/Pages/Music/Music.tsx
+++ src/Pages/Music/Music.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Music.css'
 import TitleandDescription from '../../Components/titleanddescription/TitleandDescription';
-
+import Minibutton from '../../Components/minibutton/Minibutton';
+import {GiGuitarHead, GiLovers ,GiAudioCassette, GiLockedHeart} from 'react-icons/gi'
 function Music(){
       return(
             <div className='music'>
                  <TitleandDescription titulo='mis proyectos musicales 🎸' sub='Mi historia como musico 🥁' text='Eh tocado musica desde que era chiquito, pero el momento en el que lo tome enserio, fue en la prepa, en una clase de composicion musical. Y desde esas primeras clases decidi que la musica seria una parte importante de mi vida. Empece a componer mis canciones, las cuales subo a todas las plataformas de streaming' />
+                  <div className='music__div--minibuttoncontainer'>
+                        <Minibutton texto="Carta a mis demonios" link ="https://ps.onerpm.com/8782602508" icono={<GiGuitarHead className='icon'/>}/>
+                        <Minibutton texto="Nadie mas" link="https://ps.onerpm.com/1662875779" icono = {<GiLovers className='icon'/>}/>
+                        <Minibutton texto = "Te hice cancion" link ="https://onerpm.link/112868894212" icono = {<GiAudioCassette className='icon'/>}/>
+                        <Minibutton texto = "Promesas" link ="https://ps.onerpm.com/9021901407" icono ={<GiLockedHeart className='icon'/>}/> 
+                  </div>
            </div>
       );
 }
